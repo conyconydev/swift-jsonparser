bbf37a24d1e22f18e35f138f3c877ebc35fb2674
conyconydev <conyconydev@gmail.com>
2019-01-07 12:13:16 +0900

    update step2

--- JSONParser/JSONParser/GrammarChecker.swift
+++ JSONParser/JSONParser/GrammarChecker.swift
@@ -9,6 +9,14 @@
 import Foundation
 
 struct GrammarChecker {
+    /*호출 된 상수를 선언하고이 regex인스턴스에NSRegularExpression
+    통화가 표시되어 try?있으므로 경우 NSRegularExpression(pattern:options:), 오류가 발생 regex한다
+     nil
+    검색 패턴 #[a-z0-9]+은 첫 번째 인수로 제공됩니다.
+    두 번째 인수는 정규식을 대소 문자를 구분하지 않도록 하는 옵션입니다 . 따라서 대문자와 소문자가 모두 포함 된 해시 태그를 찾습니다.*/
+    let regex = try? NSRegularExpression(pattern: "#[a-z0-9]+", options: .caseInsensitive)
+    
+    
     // 1. { 라면 : Object 라서 dictionary 인지 확인
     // 2. [ 라면 : Array 라서  Objcet인지 확인 ,
     // 3. vlaue : String, number , Object, Array , Bool , null 확인
